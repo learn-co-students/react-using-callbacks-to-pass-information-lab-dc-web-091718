c1d0090048d3b9a2e6b86909f16e2ce041c9c516
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-11-20 10:12:21 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+  clickFn = () => {
+    debugger
+    const newColor = this.props.getColor()
+    this.setState({
+      color: newColor
+    })
+  }
+
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" onClick={this.clickFn} style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
-  
+
 }
